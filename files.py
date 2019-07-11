0d0eb6f2a6ec6ac68089945be3d1d41b4bd06acc
dscastillol <38595705+dscastillol@users.noreply.github.com>
2019-07-10 22:00:46 -0500

    final

--- files.py
+++
@@ -1,5 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(r'C:\Users\davyd\Documents\cancer-project\descriptions') if isfile(join(r'C:\Users\davyd\Documents\cancer-project\descriptions', f))]
-
-print(onlyfiles)
